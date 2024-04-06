1e943191e3dc42b07dfe23496063b3afe39e6846
K-dash <kdash11011227@gmail.com>
2024-04-06 12:06:16 +0900

    Add URL query parameters to /kitchen/schedules/ API endpoint and implement data validation
     Changes to be committed:
    	modified:   src/kitchen/api/api.py

--- src/kitchen/api/api.py
+++ src/kitchen/api/api.py
@@ -1,6 +1,9 @@
+import copy
 import uuid
 from datetime import datetime
 from zoneinfo import ZoneInfo
+
+from flask import abort
 
 from flask.views import MethodView
 from flask_smorest import Blueprint
@@ -11,19 +14,21 @@ from src.kitchen.api.schemas import (
     ScheduleStatusSchema,
     GetKitchenScheduleParametars,
 )
+from marshmallow import ValidationError
 
 # flask-somorestのBlueprintクラスのインスタンスを作成
 blueprint = Blueprint("kitchen", __name__, description="Kitchen API")
 
-# 一旦、仮のレスポンスをハードコードする
-schedules = [
-    {
-        "id": uuid.uuid4(),
-        "schedule": datetime.now(ZoneInfo("Asia/Tokyo")),
-        "status": "pending",
-        "order": [{"product": "hamburger", "quantity": 1, "size": "small"}],
-    }
-]
+# インメモリに作成したスケジュールを格納するリスト
+schedules = []
+
+# データ検証用関数
+def validate_schedule(schedule):
+    schedule = copy.deepcopy(schedule)
+    schedule["scheduled"] = schedule["scheduled"].isoformat()
+    errors = GetScheduledOrderSchema().validate(schedule)
+    if errors:
+        raise ValidationError(errors)
 
 
 # Blueprintのroute()デコレータを使ってクラスベースのルーティングを定義
@@ -34,6 +39,10 @@ class KitchenSchedules(MethodView):
     # Blueprintのresponse()デコレータを使ってレスポンスペイロードのmarshmallowモデル定義
     @blueprint.response(status_code=200, schema=GetScheduledOrdersSchema)
     def get(self, parameters):
+        # DBから取得したデータを検証
+        for schedule in schedules:
+            validate_schedule(schedule)
+
         if not parameters:
             return {"schedules": schedules}
         # queryパラメータをもとにクエリを絞り込む
@@ -68,7 +77,13 @@ class KitchenSchedules(MethodView):
     # Blueprintのargments()デコレーターを使ってリクエストペイロードのmarshmallowモデルを定義
     @blueprint.arguments(ScheduleOrderSchema) #scheduleOrderSchemaモデルの定義に従ってリクエストペイロードの検証とマーシャリングが行われる
     @blueprint.response(status_code=201, schema=GetScheduledOrderSchema)
-    def post(self):
+    def post(self, payload):
+        # レスポンスを作る
+        payload["id"] = str(uuid.uuid4())
+        payload["scheduled"] = datetime.now(ZoneInfo("Asia/Tokyo"))
+        payload["status"] = "pending"
+        schedules.append(payload)
+        validate_schedule(payload)
         return schedules[0]
 
 
@@ -76,26 +91,48 @@ class KitchenSchedules(MethodView):
 class KitchenSchedule(MethodView):
     @blueprint.response(status_code=200, schema=GetScheduledOrderSchema)
     def get(self, schedule_id):
-        return schedules[0]
+        for schedule in schedules:
+            if schedule["id"] == schedule_id:
+                validate_schedule(schedule)
+                return schedule
+        abort(404, description=f"Resource with ID {schedule_id} not found")
 
     @blueprint.arguments(ScheduleOrderSchema)
     @blueprint.response(status_code=200, schema=GetScheduledOrderSchema)
     def put(self, payload, schedule_id):
-        return schedules[0]
+        for schedule in schedules:
+            if schedule["id"] == schedule_id:
+                schedule.update(payload)
+                validate_schedule(schedule)
+                return schedule
+        abort(404, description=f"Resource with ID {schedule_id} not found")
 
     @blueprint.response(status_code=204)
     def delete(self, schedule_id):
-        return
+        for index, schedule in enumerate(schedules):
+            if schedule["id"] == schedule_id:
+                schedules.pop(index)
+                return
+        abort(404, description=f"Resource with ID {schedule_id} not found")
 
 
 # 関数ベースのビューのルーティングを定義
 @blueprint.response(status_code=200, schema=GetScheduledOrderSchema)
 @blueprint.route("/kitchen/schedules/<schedule_id>/cancel", methods=["POST"])
 def cancel_schedule(schedule_id):
-    return schedules[0]
+    for schedule in schedules:
+        if schedule["id"] == schedule_id:
+            schedule["status"] = "cancelled"
+            validate_schedule(schedule)
+            return schedule
+    abort(404, description=f"Resource with ID {schedule_id} not found")
 
 
 @blueprint.response(status_code=200, schema=ScheduleStatusSchema)
 @blueprint.route("/kitchen/schedules/<schedule_id>/status", methods=["GET"])
 def get_schedule_status(schedule_id):
-    return schedules[0]
+    for schedule in schedules:
+        if schedule["id"] == schedule_id:
+            validate_schedule(schedule)
+            return {"status": schedule["status"]}
+    abort(404, description=f"Resource with ID {schedule_id} not found")
