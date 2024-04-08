9b10ba19f599d59966d7c4d4edaf7d134293e669
K-dash <kdash11011227@gmail.com>
2024-04-08 10:14:47 +0900

    Create initial Alembic migration for orders and order items models
    - Updated .gitignore to exclude certain files and directories from version control.
    - Updated .python-version to specify the Python version to be used for this project.
    
     Changes to be committed:
    	modified:   .gitignore
    	modified:   .python-version
    	modified:   pyproject.toml
    	modified:   requirements-dev.lock
    	modified:   requirements.lock
    	new file:   src/orders/alembic.ini
    	new file:   src/orders/migrations/README
    	new file:   src/orders/migrations/env.py
    	new file:   src/orders/migrations/script.py.mako
    	new file:   src/orders/migrations/versions/df585c567a17_initial_migration.py
    	new file:   src/orders/repository/models.py

---
+++ src/orders/repository/models.py
@@ -0,0 +1,59 @@
+import uuid
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()   # 宣言的なベースモデルを作成
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+
+# すべてのモデルがBaseを継承する必要がある
+class OrderModel(Base):
+    __tablename__ = "orders"
+
+    # 各プロパティはColumnクラスを使ってデータベースの列にマッピングされる
+    id = Column(String, primary_key=True, default=generate_uuid)
+    status = Column(String, nullable=False, default="created")
+    created = Column(DateTime, default=datetime.now(ZoneInfo("Asia/Tokyo")))
+    schedule_id = Column(String)
+    delivery_id = Column(String)
+    # relationship()メソッドを使ってOrderItemModelと1対多の関係を定義
+    # OrderModelから"order"というプロパティを通じてOrderItemModelにアクセスが可能になる
+    items = relationship("OrderItemModel", backref="order")
+
+    # オブジェクトをDict形式にレンダリングするためのカスタムメソッド
+    def dict(self):
+        return {
+            "id": self.id,
+            "items": [item.dict() for item in self.items],
+            "status": self.status,
+            "created": self.created,
+            "schedule_id": self.schedule_id,
+            "delivery_id": self.delivery_id,
+        }
+
+
+class OrderItemModel(Base):
+    __tablename__ = "order_items"
+
+    id = Column(String, primary_key=True, default=generate_uuid)
+    # 外部キーとして、ordersテーブルのidを参照する
+    order_id = Column(String, ForeignKey("orders.id"))
+    product = Column(String, nullable=False)
+    size = Column(String, nullable=False)
+    quantity = Column(Integer, nullable=False)
+
+    # オブジェクトをDict形式にレンダリングするためのカスタムメソッド
+    def dict(self):
+        return {
+            "id": self.id,
+            "order_id": self.order_id,
+            "product": self.product,
+            "size": self.size,
+            "quantity": self.quantity,
+        }
